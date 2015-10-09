2919d01469d68abd2c937f6c67a29af5449b7538
Klaus Nygård <klaus.nygard@gmail.com>
2015-10-09 10:06:21 +0300

    added a demo with custom message element

---
+++ demos/demo1/Demo.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import Loader from 'react-loader-advanced';
+
+
+const customMessageElement = (
+  <div>custom message element</div>
+);
+
+const Demo = React.createClass({
+  render() {
+    return (
+      <div>
+        <Loader show={true} message={customMessageElement}>
+          <div style={{padding: 30}}>
+            demo content
+          </div>
+        </Loader>
+      </div>
+    );
+  }
+});
+
+export default Demo;
