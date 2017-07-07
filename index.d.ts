339b6923d3f5552365fdbbd3d031f637c6272732
Erik Soehnel <eriksoehnel@googlemail.com>
2017-07-07 09:57:27 +0200

    add typescript type definitions

---
+++ index.d.ts
@@ -0,0 +1,29 @@
+import * as React from 'react'
+
+declare module 'react-loader-advanced' {
+
+    interface TransitionConfig {
+        transitionName: string
+        transitionEnterTimeout: number
+        transitionLeaveTimeout: number
+    }
+
+    interface Props {
+        backgroundStyle?: React.CSSProperties
+        children?: React.ReactNode
+        className?: string
+        contentBlur?: number,
+        contentStyle?: React.CSSProperties
+        disableDefaultStyles?: boolean
+        foregroundStyle?: React.CSSProperties
+        hideContentOnLoad?: boolean
+        message?: React.ReactNode
+        messageStyle?: React.CSSProperties
+        priority?: number,
+        show: boolean
+        style?: React.CSSProperties
+        transitionConfig?: TransitionConfig
+    }
+
+    export default class Loader extends React.Component<Props, {}> { }
+}
